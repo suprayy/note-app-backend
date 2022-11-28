0a455af73f724a1cf5d6990e3f42b0672c79c140
Supriyono <supraayyy@gmail.com>
2022-11-28 15:03:20 +0700

    note app backend

---
+++ pyrhon-belajar/cheatsheet.py
@@ -0,0 +1,22 @@
+# # FUNCTION DAN PERCABANGAN
+# def grade_converter(grade):
+#     if grade >= 90:
+#         return "A"
+#     elif grade >= 80:
+#         return "B"
+#     elif grade >= 70:
+#         return "C"
+#     elif grade >= 65:
+#         return "D"
+#     else:
+#         return "F"
+
+# #Memasukan nilai
+# raw_input("Enter a word:")
+
+# #gunakan akar
+# import math
+# print math.sqrt(25)
+
+# # %d akan diisi nilai list_length
+# print "There are %d items in the suitcase." % (list_length)
